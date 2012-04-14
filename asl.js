fa1b0370e02b298b07c38b8d9d6f852cf6df97d7
Narayanan Ramakrishnan <Narayanan.Ramakrishnan@gmail.com>
2012-04-14 08:18:59 -0700

    If a word is double clicked (its gets auto selected) and the ASL widget is seen. Event listener now on dblclick. Now the code exposes a ASL object which has setup and options properties exposed.

--- asl.js
+++ asl.js
@@ -1,36 +1,30 @@
-/**
-			 * Copyright (c) 2009 Mark S. Kolich
-			 * http://mark.kolich.com
-			 *
-			 * Permission is hereby granted, free of charge, to any person
-			 * obtaining a copy of this software and associated documentation
-			 * files (the "Software"), to deal in the Software without
-			 * restriction, including without limitation the rights to use,
-			 * copy, modify, merge, publish, distribute, sublicense, and/or sell
-			 * copies of the Software, and to permit persons to whom the
-			 * Software is furnished to do so, subject to the following
-			 * conditions:
-			 *
-			 * The above copyright notice and this permission notice shall be
-			 * included in all copies or substantial portions of the Software.
-			 *
-			 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-			 * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-			 * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-			 * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-			 * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-			 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-			 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-			 * OTHER DEALINGS IN THE SOFTWARE.
-			 
-			 This code was modified by Narayanan Ramakrishnan to create a user script that can show ASL representations for words
-*/
-
-
+var ASL = {};
+ASL = 
 (function(){
-	var ASL = {};
-	ASL.Selector = {};
-	ASL.Selector.getSelected = function(e){
+
+	var selector = {};
+	var utils = {addListener:null, removeListener:null};
+	var showASLTimer;
+	var options = {};
+	var setup = function (){}
+	var defaults = {};
+
+	if(typeof window.addEventListener === 'function'){
+		utils.addListener = function(el, type, callback,flag){
+			if(flag === undefined) flag = false;
+			el.addEventListener(type,callback,flag);
+		}
+	}else if(typeof window.attachEvent === 'function'){
+		utils.addListener = function(el, type, callback){
+			el.attachEvent(type,callback);
+		} 
+	}else{
+		utils.addListener = function(el, type, callback){
+			el['on'+type] = callback;
+		}
+	}
+	
+	selector.getSelected = function(e){
 		var t = '';
 		if(window.getSelection){
 			t = window.getSelection();
@@ -42,56 +36,61 @@
 		return t;
 	}
 
-	ASL.Selector.mouseup = function(e){
-		var str = String(ASL.Selector.getSelected(e));
-		//console.log("in Kolich.Selector.mouseup" + pos.x +":"+pos.y);
+	selector.showASL = function(e){
+		var str = String(selector.getSelected(e));
 		if(str != ''){
 			// trim spaces
 			str = str.replace(/^\s+|\s+$/g,"");
 			str = str.replace(/[\.\!\?\,\)\(\]\[\-\!]/g,"");
+			var src = "http://cats.gatech.edu/cats/MySignLink/dictionary/html/pages/"+str.toLowerCase()+".htm";
             clearTimeout(showASLTimer);			
-			document.getElementById("imgWrapper").innerHTML = 
-			"<div id='aslWrapper' style='background-color:white; opacity=1; width: 240px; height:200px; right:10px; position:fixed; background-position:center;background-repeat:no-repeat; -moz-border-radius: 6px; z-index:1000; border: 2px black solid;'><div style='width:240px; height:200px; text-align:center'><iframe id='iframe-0' style='margin-left:-10px;' class=' iframe-delta-0' height='180' width='220' frameborder='0' scrolling='no' name='iframe-0' src='http://cats.gatech.edu/cats/MySignLink/dictionary/html/pages/"+str.toLowerCase()+".htm'/></div></div>";
+			var imgWrapper = document.getElementById("asl-img-wrapper");
+			var html = "<div id='asl-wrapper' style='background-color:white; width: 240px; height:200px; right:10px; position:fixed; text-align:center; z-index:10; border: 1px solid #666;'><a style='position:absolute; top:2px; right:15px; z-index:20' href='"+src+"'>See More</a>";
+			html += "<iframe id='asl-iframe' style='margin-right:10px;height:180px; width:220px; border:none' scrolling='no' name='asl-iframe' src='"+src+"'/>"
+			html += "</div>	";
+
+			imgWrapper.innerHTML = html;
+			imgWrapper.style.display = 'block';
 			
-			document.getElementById('imgWrapper').style.display = 'block';
-			document.getElementById('iframe-0').style.display = 'none';
+			document.getElementById('asl-iframe').style.display = 'none';
 			
-			var iframe = document.getElementById("iframe-0");
+			var iframe = document.getElementById("asl-iframe");
 			var loadImg = document.createElement('img');
-			var imgWrap = document.getElementById("imgWrapper");
-			loadImg.setAttribute('id', 'loadImg');
+			loadImg.setAttribute('id', 'asl-load-img');
 			loadImg.setAttribute('src', 'http://naduism.com/hacks/ASL/loading.gif');
 			loadImg.style.position = 'fixed';
 			loadImg.style.right = '30px';
 			loadImg.style.top = '50px';
 			
-			document.getElementById("aslWrapper").appendChild(loadImg);
-			
+			var aslWrapper = document.getElementById("asl-wrapper");
+			aslWrapper.appendChild(loadImg);
+						
 			setTimeout(function(){
-				var loadImg = document.getElementById('loadImg');
-				document.getElementById("aslWrapper").removeChild(loadImg); 
-				document.getElementById('iframe-0').style.display = 'inline';
+				var loadImg = document.getElementById('asl-load-img');
+				aslWrapper.removeChild(loadImg); 
+				document.getElementById('asl-iframe').style.display = 'inline';
 			},1000);
-			showASLTimer = setTimeout(function(){document.getElementById('imgWrapper').style.display = 'none';},7500);
+			showASLTimer = setTimeout(function(){imgWrapper.style.display = 'none';},7500);
 		}
 	};
-	var showASLTimer;
-			
-	ASL.Selector.init = function(){
-		console.log(ASL);
-		if(document.addEventListener){
-			document.addEventListener("mouseup", ASL.Selector.mouseup, true);
-		}
-		else if(document.attachEvent){
-			document.attachEvent("mouseup", ASL.Selector.mouseup);
-		}
+	
+	var init = function(){
+		utils.addListener(document, "dblclick", selector.showASL, false);
 		var imgWrap = document.createElement('div');
-		imgWrap.setAttribute('id', 'imgWrapper');
+		imgWrap.setAttribute('id', 'asl-img-wrapper');
 		document.body.insertBefore(imgWrap,document.body.childNodes[0]);
-		document.getElementById('imgWrapper').style.display = 'none';
+		imgWrap.style.display = 'none';
+		setup(options);
 		//console.log(window.location);
 	};
-	window.onload = ASL.Selector.init;	
+	window.onload = init;	
+
+
+
+	return {
+		setup: setup,
+		options:options
+	}
 
 }());
 
